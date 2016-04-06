1e488292529b1a3defeddc71c9210c712edffc15
SunayaShivakumar <sshivak2@illinois.edu>
2016-04-06 16:41:39 -0500

    new file

--- app/views.py
+++ app/views.py
@@ -61,7 +61,7 @@ def handle_survey_page1():
         topItemsByStyleWord = getItemsByStyle(topics,phiMatrices,"concrete")
         t1 = topItemsByStyleWord.copy()
         print(topItemsByStyleWord)
-        #return jsonify(topItemsByStyleWord)
+        return jsonify(topItemsByStyleWord)
 
 @app.route('/page2', methods=['GET', 'POST'])
 def survey_page2():
